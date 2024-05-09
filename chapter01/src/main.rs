ad0873ffd7895a863b0e34cd36033100ea35b37f
Ferris <ferris.evans@outlook.com>
2024-05-09 16:03:29 +0800

    ch01

--- chapter01/src/main.rs
+++ chapter01/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    ch01();
+}
+
+fn ch01() {
+    let x = 1;
+    // x = 2; error!
+    println!("x: {}", x);
+    let mut y = 2;
+    println!("y: {}", y);
+    y = 3;
+    println!("y: {}", y);
 }
